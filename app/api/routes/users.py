74012d91e625badf9ed335b3114e3640ab8f79b8
taofeeadisa <adisataofeekabiodun@gmail.com>
2024-11-23 01:26:28 +0200

    resolve conflict

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -18,33 +18,25 @@ async def login(user:Annotated[UserLogin, Form()]):
    response = user_crud.login(user)
    return response
 
-
-<<<<<<< HEAD
-@user_router.get("/users")
-=======
 @user_router.get("/users",  status_code=status.HTTP_200_OK)
->>>>>>> 40ee1b3030d940c28f6f7b92c1e102e1239c7997
+
 def get_users(user_params: Annotated[UserParams, Query()]):
     users_data = user_crud.get_users(user_params)
     return users_data
 
 
-<<<<<<< HEAD
-@user_router.put("/users/{user_Id}")
-=======
+
 @user_router.put("/users/{user_Id}", status_code=status.HTTP_201_CREATED)
->>>>>>> 40ee1b3030d940c28f6f7b92c1e102e1239c7997
+
 def update_profile(user_Id:str, update_profile:UserUpdate):
     updated_profile = user_crud.update_profile(user_Id, update_profile)
     
     return updated_profile
     
     
-<<<<<<< HEAD
-@user_router.delete("/users/{user_Id}")
-=======
+
+
 @user_router.delete("/users/{user_Id}", status_code=status.HTTP_204_NO_CONTENT)
->>>>>>> 40ee1b3030d940c28f6f7b92c1e102e1239c7997
 def delete_profile(user_Id:str):
     profile_deleted = user_crud.delete_profile(user_Id)
     
